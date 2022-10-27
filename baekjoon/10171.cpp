d6288897830edab99d3a38fc7c40fb4fd1ab9349
objectio <objection@kakao.com>
2022-10-27 23:53:44 +0900

    [ADD] 진짜 기본 중의 기본ㅋㅋ

---
+++ baekjoon/10171.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    printf("%s\n", "\\    /\\");  
+    printf("%s\n", " )  ( ')");
+    printf("%s\n", "(  /  )");
+    printf("%s\n", " \\(__)|");
+
+    return 0;
+}
